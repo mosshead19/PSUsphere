715da6fbaf71b1047fcd022a4d154cdb51d394a6
Ritvent <ivanventurilloq@gmail.com>
2025-08-20 10:13:35 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
@@ -25,3 +21,28 @@ class OrgMemberAdmin(admin.ModelAdmin):
             return member.program
         except Student.DoesNotExist:
             return None
+        
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")   
+
+    search_fields = ("name", "description",)
+
+    list_filter = ("college",)
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college",)
+
+    search_fields = ("prog_name", "college",)
+
+    list_filter = ("college",)
+
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at",)
+
+    search_fields = ("college_name",)
+
+    list_filter = ("created_at",)
+
