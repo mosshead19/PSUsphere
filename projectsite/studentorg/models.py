9da9825692ab190814602690acce965f10d6025f
Ritvent <ivanventurilloq@gmail.com>
2025-09-02 12:37:06 +0800

    fix delete org member

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -43,3 +43,7 @@ class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     date_joined = models.DateField()
+
+
+    def __str__(self):
+        return f"{self.student.lastname}, {self.student.firstname}"
