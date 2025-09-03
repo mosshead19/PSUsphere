12fc4e82e487507e3940d398bc85efcd87b256d0
Ritvent <ivanventurilloq@gmail.com>
2025-09-03 08:49:55 +0800

    update forms.py

--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -1,8 +1,29 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Organization
+from .models import Program
+from .models import Student
+from .models import OrgMember
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
+        fields = "__all__"
+
+
+class ProgramForm(ModelForm):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+
+class OrganizationMemberForm(ModelForm):
+    class Meta:
+        model = OrgMember
         fields = "__all__"
