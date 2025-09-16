a4d57bf9727161e6051fc97f95c1e99d518ce4a6
Ritvent <ivanventurilloq@gmail.com>
2025-09-17 00:19:08 +0800

    edit context home

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -7,12 +7,30 @@ from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
 
 from studentorg.models import OrgMember, Student, College, Program
-
+from django.utils import timezone
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 class OrganizationList(ListView):
     model = Organization
